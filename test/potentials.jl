a0ab5eedb8f644c9b8ae5fb917e99f4f70d8716a
Gnimuc <GnimucKey@gmail.com>
2017-01-05 15:20:53 +0800

    Use @testset instead of hard-coded info

--- test/potentials.jl
+++ test/potentials.jl
@@ -3,7 +3,6 @@ import NRIRHOPM: sum_absolute_diff, sum_squared_diff,
                  topology_preserving, jᶠᶠ, jᵇᶠ, jᶠᵇ, jᵇᵇ, jᶠᶠᶠ, jᵇᶠᶠ, jᶠᵇᶠ, jᵇᵇᶠ,
                  jᶠᶠᵇ, jᵇᶠᵇ, jᶠᵇᵇ, jᵇᵇᵇ
 
-# construct simple 0-1 images
 targetImage = Float64[1 0 1;
                       0 1 0;
                       0 1 1]
@@ -12,271 +11,264 @@ sourceImage = Float64[1 0 1;
                       0 1 0;
                       1 1 0]
 
-# create transform vectors
 labels = [(i,j) for i in -1:1, j in -1:1]
 
-# test for sum_absolute_diff
-info("Testing sum_absolute_diff:")
-cost = sum_absolute_diff(targetImage, sourceImage, labels)
-@test all(cost .>= 0)
+@testset "potentials" begin
+    @testset "sum_absolute_diff" begin
+        cost = sum_absolute_diff(targetImage, sourceImage, labels)
+        @test all(cost .>= 0)
 
-mat = reshape(cost, length(targetImage), length(labels))
-dims = size(targetImage)
-for 𝒊 in CartesianRange(dims)
-    i = sub2ind(dims, 𝒊.I...)
-    for a in find(mat[i,:] .== maximum(mat[i,:]))
-        𝐭 = CartesianIndex(labels[a])
-        @test targetImage[𝒊] == sourceImage[𝒊+𝐭]
+        mat = reshape(cost, length(targetImage), length(labels))
+        dims = size(targetImage)
+        for 𝒊 in CartesianRange(dims)
+            i = sub2ind(dims, 𝒊.I...)
+            for a in find(mat[i,:] .== maximum(mat[i,:]))
+                𝐭 = CartesianIndex(labels[a])
+                @test targetImage[𝒊] == sourceImage[𝒊+𝐭]
+            end
+        end
+    end
+
+    @testset "sum_squared_diff" begin
+        cost = sum_squared_diff(targetImage, sourceImage, labels)
+        @test all(cost .>= 0)
+
+        mat = reshape(cost, length(targetImage), length(labels))
+        dims = size(targetImage)
+        for 𝒊 in CartesianRange(dims)
+            i = sub2ind(dims, 𝒊.I...)
+            for a in find(mat[i,:] .== maximum(mat[i,:]))
+                𝐭 = CartesianIndex(labels[a])
+                @test targetImage[𝒊] == sourceImage[𝒊+𝐭]
+            end
+        end
+    end
+
+    @testset "potts_model" begin
+        # 2D
+        fp = (1,2)
+        fq = fp
+        d = rand()
+        @test potts_model(fp, fq, d) == 0
+        fq = (3,4)
+        @test potts_model(fp, fq, d) == d
+
+        # 3D
+        fp = (1,2,3)
+        fq = fp
+        d = rand()
+        @test potts_model(fp, fq, d) == 0
+        fq = (2,3,4)
+        @test potts_model(fp, fq, d) == d
+    end
+
+    @testset "truncated_absolute_diff" begin
+        # 2D
+        fp = tuple(rand(2)...)
+        fq = tuple(rand(2)...)
+
+        cost = truncated_absolute_diff(fp, fq, 1, Inf)
+        delta = abs(vecnorm([fp...] - [fq...]))
+        @test cost == delta
+
+        rate = rand()
+        @test truncated_absolute_diff(fp, fq, rate, Inf) == rate * delta
+
+        # d = 0
+        @test truncated_absolute_diff(fp, fq, 2, 0.0) == 0
+
+        # 3D
+        fp = tuple(rand(3)...)
+        fq = tuple(rand(3)...)
+
+        cost = truncated_absolute_diff(fp, fq, 1, Inf)
+        delta = abs(vecnorm([fp...] - [fq...]))
+        @test cost == delta
+
+        rate = rand()
+        @test truncated_absolute_diff(fp, fq, rate, Inf) == rate * delta
+
+        # d = 0
+        @test truncated_absolute_diff(fp, fq, 2, 0) == 0
+    end
+
+    @testset "truncated_quadratic_diff" begin
+        # 2D
+        fp = tuple(rand(2)...)
+        fq = tuple(rand(2)...)
+
+        cost = truncated_quadratic_diff(fp, fq, 1, Inf)
+        delta = vecnorm([fp...] - [fq...])^2
+        @test cost - delta < 1e-10
+
+        rate = rand()
+        @test truncated_quadratic_diff(fp, fq, rate, Inf) - rate * delta < 1e-10
+
+        # d = 0
+        @test truncated_quadratic_diff(fp, fq, 2, 0) == 0
+
+        # 3D
+        fp = tuple(rand(3)...)
+        fq = tuple(rand(3)...)
+
+        cost = truncated_quadratic_diff(fp, fq, 1, Inf)
+        delta = vecnorm([fp...] - [fq...])^2
+        @test cost - delta < 1e-10
+
+        rate = rand()
+        @test truncated_quadratic_diff(fp, fq, rate, Inf) - rate * delta < 1e-10
+
+        # d = 0
+        @test truncated_quadratic_diff(fp, fq, 2, 0) == 0
+    end
+
+    @testset "topology_preserving" begin
+        # topology_preserving                        y
+        #   □ ▦ □        ▦                ▦          ↑        ⬔ => p1 => a
+        #   ⬓ ⬔ ⬓  =>  ⬓ ⬔   ⬓ ⬔    ⬔ ⬓   ⬔ ⬓        |        ⬓ => p2 => b
+        #   □ ▦ □              ▦    ▦          (x,y):+--> x   ▦ => p3 => c
+        #              Jᵇᶠ   Jᵇᵇ    Jᶠᵇ   Jᶠᶠ
+
+        # jᶠᶠ, jᵇᶠ, jᶠᵇ, jᵇᵇ
+        #   □ ⬓ □        ⬓                ⬓      r,c-->    ⬔ => p1 => a
+        #   ▦ ⬔ ▦  =>  ▦ ⬔   ▦ ⬔    ⬔ ▦   ⬔ ▦    |         ⬓ => p2 => b
+        #   □ ⬓ □              ⬓    ⬓            ↓         ▦ => p3 => c
+        #              Jᵇᵇ   Jᶠᵇ    Jᶠᶠ   Jᵇᶠ
+
+        # test for Jᵇᶠ
+        p1 = rand(0:256, 2)
+        p2 = p1 - [1,0]
+        p3 = p1 + [0,1]
+
+        a, b, c = [1,1], [0,-1], [-1,1]
+        @test topology_preserving(p2, p1, p3, b, a, c) == 0
+        @test jᵇᶠ(tuple(a...), tuple(b...), tuple(c...)) == 0
+
+        a, b, c = [-1,-1], [0,-1], [-1,1]
+        @test topology_preserving(p2, p1, p3, b, a, c) == 1
+        @test jᵇᶠ(tuple(a...), tuple(b...), tuple(c...)) == 1
+
+        for i = 1:1000
+            a, b, c = rand(-15:15, 2), rand(-15:15, 2), rand(-15:15, 2)
+            @test topology_preserving(p2, p1, p3, b, a, c) == jᵇᶠ(tuple(a...), tuple(b...), tuple(c...))
+        end
+
+        # test for Jᵇᵇ
+        p1 = rand(0:256, 2)
+        p2 = p1 - [1,0]
+        p3 = p1 - [0,1]
+
+        a, b, c = [1,-1], [0,0], [0,0]
+        @test topology_preserving(p2, p1, p3, b, a, c) == 0
+        @test jᵇᵇ(tuple(a...), tuple(b...), tuple(c...)) == 0
+
+        a, b, c = [-1,-1], [0,0], [0,0]
+        @test topology_preserving(p2, p1, p3, b, a, c) == 1
+        @test jᵇᵇ(tuple(a...), tuple(b...), tuple(c...)) == 1
+
+        for i = 1:1000
+            a, b, c = rand(-15:15, 2), rand(-15:15, 2), rand(-15:15, 2)
+            @test topology_preserving(p2, p1, p3, b, a, c) == jᵇᵇ(tuple(a...), tuple(b...), tuple(c...))
+        end
+
+        # test for Jᶠᵇ
+        p1 = rand(0:256, 2)
+        p2 = p1 + [1,0]
+        p3 = p1 - [0,1]
+
+        a, b, c = [-1,1], [0,0], [0,0]
+        @test topology_preserving(p2, p1, p3, b, a, c) == 0
+        @test jᶠᵇ(tuple(a...), tuple(b...), tuple(c...)) == 0
+
+        a, b, c = [1,-1], [0,0], [0,0]
+        @test topology_preserving(p2, p1, p3, b, a, c) == 1
+        @test jᶠᵇ(tuple(a...), tuple(b...), tuple(c...)) == 1
+
+        for i = 1:1000
+            a, b, c = rand(-15:15, 2), rand(-15:15, 2), rand(-15:15, 2)
+            @test topology_preserving(p2, p1, p3, b, a, c) == jᶠᵇ(tuple(a...), tuple(b...), tuple(c...))
+        end
+
+        # test for Jᶠᶠ
+        p1 = rand(0:256, 2)
+        p2 = p1 + [1,0]
+        p3 = p1 + [0,1]
+
+        a, b, c = [-1,-1], [0,0], [0,0]
+        @test topology_preserving(p2, p1, p3, b, a, c) == 0
+        @test jᶠᶠ(tuple(a...), tuple(b...), tuple(c...)) == 0
+
+        a, b, c = [1,1], [0,0], [0,0]
+        @test topology_preserving(p2, p1, p3, b, a, c) == 1
+        @test jᶠᶠ(tuple(a...), tuple(b...), tuple(c...)) == 1
+
+        for i = 1:1000
+            a, b, c = rand(-15:15, 2), rand(-15:15, 2), rand(-15:15, 2)
+            @test topology_preserving(p2, p1, p3, b, a, c) == jᶠᶠ(tuple(a...), tuple(b...), tuple(c...))
+        end
+
+        # topology preserving in 3D(just some trivial tests)
+        # coordinate system(r,c,z):
+        #  up  r     c --->        z × × (front to back)
+        #  to  |   left to right     × ×
+        # down ↓
+        # coordinate => point => label:
+        # iii => p1 => α   jjj => p2 => β   kkk => p3 => χ   mmm => p5 => δ
+
+        # test for Jᶠᶠᶠ
+        a, b, c, d = (0,0,0), (0,0,0), (0,0,0), (0,0,0)
+        @test jᶠᶠᶠ(a,b,c,d) == 0
+
+        a, b, c, d = (1,1,1), (0,0,0), (0,0,0), (0,0,0)
+        @test jᶠᶠᶠ(a,b,c,d) == 1
+
+        # test for Jᵇᶠᶠ
+        a, b, c, d = (0,0,0), (0,0,0), (0,0,0), (0,0,0)
+        @test jᵇᶠᶠ(a,b,c,d) == 0
+
+        a, b, c, d = (-1,1,1), (0,0,0), (0,0,0), (0,0,0)
+        @test jᵇᶠᶠ(a,b,c,d) == 1
+
+        # test for Jᶠᵇᶠ
+        a, b, c, d = (0,0,0), (0,0,0), (0,0,0), (0,0,0)
+        @test jᶠᵇᶠ(a,b,c,d) == 0
+
+        a, b, c, d = (1,-1,1), (0,0,0), (0,0,0), (0,0,0)
+        @test jᶠᵇᶠ(a,b,c,d) == 1
+
+        # test for Jᵇᵇᶠ
+        a, b, c, d = (0,0,0), (0,0,0), (0,0,0), (0,0,0)
+        @test jᵇᵇᶠ(a,b,c,d) == 0
+
+        a, b, c, d = (-1,-1,1), (0,0,0), (0,0,0), (0,0,0)
+        @test jᵇᵇᶠ(a,b,c,d) == 1
+
+        # test for Jᶠᶠᵇ
+        a, b, c, d = (0,0,0), (0,0,0), (0,0,0), (0,0,0)
+        @test jᶠᶠᵇ(a,b,c,d) == 0
+
+        a, b, c, d = (1,1,-1), (0,0,0), (0,0,0), (0,0,0)
+        @test jᶠᶠᵇ(a,b,c,d) == 1
+
+        # test for Jᵇᶠᵇ
+        a, b, c, d = (0,0,0), (0,0,0), (0,0,0), (0,0,0)
+        @test jᵇᶠᵇ(a,b,c,d) == 0
+
+        a, b, c, d = (-1,1,-1), (0,0,0), (0,0,0), (0,0,0)
+        @test jᵇᶠᵇ(a,b,c,d) == 1
+
+        # test for Jᶠᵇᵇ
+        a, b, c, d = (0,0,0), (0,0,0), (0,0,0), (0,0,0)
+        @test jᶠᵇᵇ(a,b,c,d) == 0
+
+        a, b, c, d = (1,-1,-1), (0,0,0), (0,0,0), (0,0,0)
+        @test jᶠᵇᵇ(a,b,c,d) == 1
+
+        # test for Jᵇᵇᵇ
+        a, b, c, d = (0,0,0), (0,0,0), (0,0,0), (0,0,0)
+        @test jᵇᵇᵇ(a,b,c,d) == 0
+
+        a, b, c, d = (-1,-1,-1), (0,0,0), (0,0,0), (0,0,0)
+        @test jᵇᵇᵇ(a,b,c,d) == 1
     end
 end
-println("Passed.")
-
-# test for sum_squared_diff
-info("Testing sum_squared_diff:")
-cost = sum_squared_diff(targetImage, sourceImage, labels)
-@test all(cost .>= 0)
-
-mat = reshape(cost, length(targetImage), length(labels))
-dims = size(targetImage)
-for 𝒊 in CartesianRange(dims)
-    i = sub2ind(dims, 𝒊.I...)
-    for a in find(mat[i,:] .== maximum(mat[i,:]))
-        𝐭 = CartesianIndex(labels[a])
-        @test targetImage[𝒊] == sourceImage[𝒊+𝐭]
-    end
-end
-println("Passed.")
-
-# test for potts_model
-info("Testing potts_model")
-# 2D
-fp = (1,2)
-fq = fp
-d = rand()
-@test potts_model(fp, fq, d) == 0
-fq = (3,4)
-@test potts_model(fp, fq, d) == d
-
-# 3D
-fp = (1,2,3)
-fq = fp
-d = rand()
-@test potts_model(fp, fq, d) == 0
-fq = (2,3,4)
-@test potts_model(fp, fq, d) == d
-println("Passed.")
-
-
-# test for truncated_absolute_diff
-info("Testing truncated_absolute_diff:")
-# 2D
-fp = tuple(rand(2)...)
-fq = tuple(rand(2)...)
-
-cost = truncated_absolute_diff(fp, fq, 1, Inf)
-delta = abs(vecnorm([fp...] - [fq...]))
-@test cost == delta
-
-rate = rand()
-@test truncated_absolute_diff(fp, fq, rate, Inf) == rate * delta
-
-# d = 0
-@test truncated_absolute_diff(fp, fq, 2, 0.0) == 0
-
-# 3D
-fp = tuple(rand(3)...)
-fq = tuple(rand(3)...)
-
-cost = truncated_absolute_diff(fp, fq, 1, Inf)
-delta = abs(vecnorm([fp...] - [fq...]))
-@test cost == delta
-
-rate = rand()
-@test truncated_absolute_diff(fp, fq, rate, Inf) == rate * delta
-
-# d = 0
-@test truncated_absolute_diff(fp, fq, 2, 0) == 0
-println("Passed.")
-
-# test for truncated_quadratic_diff
-info("Testing truncated_quadratic_diff:")
-# 2D
-fp = tuple(rand(2)...)
-fq = tuple(rand(2)...)
-
-cost = truncated_quadratic_diff(fp, fq, 1, Inf)
-delta = vecnorm([fp...] - [fq...])^2
-@test cost - delta < 1e-10
-
-rate = rand()
-@test truncated_quadratic_diff(fp, fq, rate, Inf) - rate * delta < 1e-10
-
-# d = 0
-@test truncated_quadratic_diff(fp, fq, 2, 0) == 0
-
-# 3D
-fp = tuple(rand(3)...)
-fq = tuple(rand(3)...)
-
-cost = truncated_quadratic_diff(fp, fq, 1, Inf)
-delta = vecnorm([fp...] - [fq...])^2
-@test cost - delta < 1e-10
-
-rate = rand()
-@test truncated_quadratic_diff(fp, fq, rate, Inf) - rate * delta < 1e-10
-
-# d = 0
-@test truncated_quadratic_diff(fp, fq, 2, 0) == 0
-println("Passed.")
-
-# test for topology_preserving
-info("Testing topology_preserving:")
-# topology_preserving                        y
-#   □ ▦ □        ▦                ▦          ↑        ⬔ => p1 => a
-#   ⬓ ⬔ ⬓  =>  ⬓ ⬔   ⬓ ⬔    ⬔ ⬓   ⬔ ⬓        |        ⬓ => p2 => b
-#   □ ▦ □              ▦    ▦          (x,y):+--> x   ▦ => p3 => c
-#              Jᵇᶠ   Jᵇᵇ    Jᶠᵇ   Jᶠᶠ
-
-# jᶠᶠ, jᵇᶠ, jᶠᵇ, jᵇᵇ
-#   □ ⬓ □        ⬓                ⬓      r,c-->    ⬔ => p1 => a
-#   ▦ ⬔ ▦  =>  ▦ ⬔   ▦ ⬔    ⬔ ▦   ⬔ ▦    |         ⬓ => p2 => b
-#   □ ⬓ □              ⬓    ⬓            ↓         ▦ => p3 => c
-#              Jᵇᵇ   Jᶠᵇ    Jᶠᶠ   Jᵇᶠ
-
-# test for Jᵇᶠ
-p1 = rand(0:256, 2)
-p2 = p1 - [1,0]
-p3 = p1 + [0,1]
-
-a, b, c = [1,1], [0,-1], [-1,1]
-@test topology_preserving(p2, p1, p3, b, a, c) == 0
-@test jᵇᶠ(tuple(a...), tuple(b...), tuple(c...)) == 0
-
-a, b, c = [-1,-1], [0,-1], [-1,1]
-@test topology_preserving(p2, p1, p3, b, a, c) == 1
-@test jᵇᶠ(tuple(a...), tuple(b...), tuple(c...)) == 1
-
-for i = 1:1000
-    a, b, c = rand(-15:15, 2), rand(-15:15, 2), rand(-15:15, 2)
-    @test topology_preserving(p2, p1, p3, b, a, c) == jᵇᶠ(tuple(a...), tuple(b...), tuple(c...))
-end
-
-# test for Jᵇᵇ
-p1 = rand(0:256, 2)
-p2 = p1 - [1,0]
-p3 = p1 - [0,1]
-
-a, b, c = [1,-1], [0,0], [0,0]
-@test topology_preserving(p2, p1, p3, b, a, c) == 0
-@test jᵇᵇ(tuple(a...), tuple(b...), tuple(c...)) == 0
-
-a, b, c = [-1,-1], [0,0], [0,0]
-@test topology_preserving(p2, p1, p3, b, a, c) == 1
-@test jᵇᵇ(tuple(a...), tuple(b...), tuple(c...)) == 1
-
-for i = 1:1000
-    a, b, c = rand(-15:15, 2), rand(-15:15, 2), rand(-15:15, 2)
-    @test topology_preserving(p2, p1, p3, b, a, c) == jᵇᵇ(tuple(a...), tuple(b...), tuple(c...))
-end
-
-# test for Jᶠᵇ
-p1 = rand(0:256, 2)
-p2 = p1 + [1,0]
-p3 = p1 - [0,1]
-
-a, b, c = [-1,1], [0,0], [0,0]
-@test topology_preserving(p2, p1, p3, b, a, c) == 0
-@test jᶠᵇ(tuple(a...), tuple(b...), tuple(c...)) == 0
-
-a, b, c = [1,-1], [0,0], [0,0]
-@test topology_preserving(p2, p1, p3, b, a, c) == 1
-@test jᶠᵇ(tuple(a...), tuple(b...), tuple(c...)) == 1
-
-for i = 1:1000
-    a, b, c = rand(-15:15, 2), rand(-15:15, 2), rand(-15:15, 2)
-    @test topology_preserving(p2, p1, p3, b, a, c) == jᶠᵇ(tuple(a...), tuple(b...), tuple(c...))
-end
-
-# test for Jᶠᶠ
-p1 = rand(0:256, 2)
-p2 = p1 + [1,0]
-p3 = p1 + [0,1]
-
-a, b, c = [-1,-1], [0,0], [0,0]
-@test topology_preserving(p2, p1, p3, b, a, c) == 0
-@test jᶠᶠ(tuple(a...), tuple(b...), tuple(c...)) == 0
-
-a, b, c = [1,1], [0,0], [0,0]
-@test topology_preserving(p2, p1, p3, b, a, c) == 1
-@test jᶠᶠ(tuple(a...), tuple(b...), tuple(c...)) == 1
-
-for i = 1:1000
-    a, b, c = rand(-15:15, 2), rand(-15:15, 2), rand(-15:15, 2)
-    @test topology_preserving(p2, p1, p3, b, a, c) == jᶠᶠ(tuple(a...), tuple(b...), tuple(c...))
-end
-
-# topology preserving in 3D(just some trivial tests)
-# coordinate system(r,c,z):
-#  up  r     c --->        z × × (front to back)
-#  to  |   left to right     × ×
-# down ↓
-# coordinate => point => label:
-# iii => p1 => α   jjj => p2 => β   kkk => p3 => χ   mmm => p5 => δ
-
-# test for Jᶠᶠᶠ
-a, b, c, d = (0,0,0), (0,0,0), (0,0,0), (0,0,0)
-@test jᶠᶠᶠ(a,b,c,d) == 0
-
-a, b, c, d = (1,1,1), (0,0,0), (0,0,0), (0,0,0)
-@test jᶠᶠᶠ(a,b,c,d) == 1
-
-# test for Jᵇᶠᶠ
-a, b, c, d = (0,0,0), (0,0,0), (0,0,0), (0,0,0)
-@test jᵇᶠᶠ(a,b,c,d) == 0
-
-a, b, c, d = (-1,1,1), (0,0,0), (0,0,0), (0,0,0)
-@test jᵇᶠᶠ(a,b,c,d) == 1
-
-# test for Jᶠᵇᶠ
-a, b, c, d = (0,0,0), (0,0,0), (0,0,0), (0,0,0)
-@test jᶠᵇᶠ(a,b,c,d) == 0
-
-a, b, c, d = (1,-1,1), (0,0,0), (0,0,0), (0,0,0)
-@test jᶠᵇᶠ(a,b,c,d) == 1
-
-# test for Jᵇᵇᶠ
-a, b, c, d = (0,0,0), (0,0,0), (0,0,0), (0,0,0)
-@test jᵇᵇᶠ(a,b,c,d) == 0
-
-a, b, c, d = (-1,-1,1), (0,0,0), (0,0,0), (0,0,0)
-@test jᵇᵇᶠ(a,b,c,d) == 1
-
-# test for Jᶠᶠᵇ
-a, b, c, d = (0,0,0), (0,0,0), (0,0,0), (0,0,0)
-@test jᶠᶠᵇ(a,b,c,d) == 0
-
-a, b, c, d = (1,1,-1), (0,0,0), (0,0,0), (0,0,0)
-@test jᶠᶠᵇ(a,b,c,d) == 1
-
-# test for Jᵇᶠᵇ
-a, b, c, d = (0,0,0), (0,0,0), (0,0,0), (0,0,0)
-@test jᵇᶠᵇ(a,b,c,d) == 0
-
-a, b, c, d = (-1,1,-1), (0,0,0), (0,0,0), (0,0,0)
-@test jᵇᶠᵇ(a,b,c,d) == 1
-
-# test for Jᶠᵇᵇ
-a, b, c, d = (0,0,0), (0,0,0), (0,0,0), (0,0,0)
-@test jᶠᵇᵇ(a,b,c,d) == 0
-
-a, b, c, d = (1,-1,-1), (0,0,0), (0,0,0), (0,0,0)
-@test jᶠᵇᵇ(a,b,c,d) == 1
-
-# test for Jᵇᵇᵇ
-a, b, c, d = (0,0,0), (0,0,0), (0,0,0), (0,0,0)
-@test jᵇᵇᵇ(a,b,c,d) == 0
-
-a, b, c, d = (-1,-1,-1), (0,0,0), (0,0,0), (0,0,0)
-@test jᵇᵇᵇ(a,b,c,d) == 1
-
-println("Passed.")
--- test/tensors.jl
+++ test/tensors.jl
@@ -1,79 +1,80 @@
 using TensorOperations
 import NRIRHOPM: contract
 
-info("Testing 2nd order SSTensor:")
-n = 100
-A = Symmetric(rand(n,n))
-A -= diagm(diag(A))
-x = rand(n)
+@testset "tensors" begin
+    @testset "2nd order SSTensor" begin
+        n = 100
+        A = Symmetric(rand(n,n))
+        A -= diagm(diag(A))
+        x = rand(n)
 
-data = Float64[]
-index = NTuple{2,Int}[]
-for i = 1:n, j = i+1:n
-    push!(data, A[i,j])
-    push!(index, (i,j))
-end
+        data = Float64[]
+        index = NTuple{2,Int}[]
+        for i = 1:n, j = i+1:n
+            push!(data, A[i,j])
+            push!(index, (i,j))
+        end
 
-@test contract(SSTensor(data, index, size(A)), x) ≈ tensorcontract(A, [1,2], x, [2])
-@test SSTensor(data, index, size(A)) ⊙ x ≈ tensorcontract(A, [1,2], x, [2])
-println("Passed.")
+        @test contract(SSTensor(data, index, size(A)), x) ≈ tensorcontract(A, [1,2], x, [2])
+        @test SSTensor(data, index, size(A)) ⊙ x ≈ tensorcontract(A, [1,2], x, [2])
+    end
 
-info("Testing 3rd order SSTensor:")
-n = 100
-a = rand(n)
-A = kron(a, a', a)
-A = reshape(A, n, n, n)
+    @testset "3rd order SSTensor" begin
+        n = 100
+        a = rand(n)
+        A = kron(a, a', a)
+        A = reshape(A, n, n, n)
 
-data = Float64[]
-index = NTuple{3,Int}[]
+        data = Float64[]
+        index = NTuple{3,Int}[]
 
-for i = 1:n, j = 1:n, k = 1:n
-    if i == j || i == k || j == k
-        A[i,j,k] = 0
-    elseif i < j < k
-        push!(data, A[i,j,k])
-        push!(index, (i,j,k))
+        for i = 1:n, j = 1:n, k = 1:n
+            if i == j || i == k || j == k
+                A[i,j,k] = 0
+            elseif i < j < k
+                push!(data, A[i,j,k])
+                push!(index, (i,j,k))
+            end
+        end
+
+        x = rand(n)
+        v = tensorcontract(A, [1,2,3], x, [3])
+        @test contract(SSTensor(data, index, size(A)), x) ≈ tensorcontract(v, [1,2], x, [2])
+        @test SSTensor(data, index, size(A)) ⊙ x ≈ tensorcontract(v, [1,2], x, [2])
+    end
+
+    @testset "4th order BSSTensor" begin
+        imageDims = (32,32)
+        labels = [[(i,j) for i in -2:2, j in -2:2]...]
+        ss = pairwiseclique4validation(imageDims, labels);
+        bss = pairwiseclique(imageDims, labels, TAD());
+        x = rand(prod(imageDims)*length(labels))
+        @test ss ⊙ x ≈ bss ⊙ x
+    end
+
+    @testset "6th order BSSTensor" begin
+        imageDims = (16,16)
+        labels = [[(i,j) for i in -1:1, j in -1:1]...]
+        ss = treyclique4validation(imageDims, [[[i,j] for i in -1:1, j in -1:1]...]);
+        bss = treyclique(imageDims, labels, TP());
+        x = rand(prod(imageDims)*length(labels))
+        @test ss ⊙ x ≈ bss ⊙ x
+    end
+
+    @testset "trivial interfaces" begin
+        imageDims = (8,8)
+        labels = [[(i,j) for i in -2:2, j in -2:2]...]
+        ss = pairwiseclique4validation(imageDims, labels);
+        bss = pairwiseclique(imageDims, labels, TAD());
+        pixelNum = prod(imageDims)
+        labelNum = length(labels)
+
+        @test size(ss) == (pixelNum*labelNum, pixelNum*labelNum)
+        @test length(ss) == pixelNum*labelNum*pixelNum*labelNum
+
+        @test size(bss) == (pixelNum, labelNum, pixelNum, labelNum) == size(bss.blocks[])
+        @test length(bss) == length(bss.blocks[]) == length(ss)
+
+        @test nnz(bss)*labelNum^2 == nnz(ss)
     end
 end
-
-x = rand(n)
-v = tensorcontract(A, [1,2,3], x, [3])
-@test contract(SSTensor(data, index, size(A)), x) ≈ tensorcontract(v, [1,2], x, [2])
-@test SSTensor(data, index, size(A)) ⊙ x ≈ tensorcontract(v, [1,2], x, [2])
-println("Passed.")
-
-info("Testing 4th order BSSTensor:")
-imageDims = (32,32)
-labels = [[(i,j) for i in -2:2, j in -2:2]...]
-ss = pairwiseclique4validation(imageDims, labels);
-bss = pairwiseclique(imageDims, labels, TAD());
-x = rand(prod(imageDims)*length(labels))
-@test ss ⊙ x ≈ bss ⊙ x
-println("Passed.")
-
-info("Testing 6th order BSSTensor:")
-imageDims = (16,16)
-labels = [[(i,j) for i in -1:1, j in -1:1]...]
-ss = treyclique4validation(imageDims, [[[i,j] for i in -1:1, j in -1:1]...]);
-bss = treyclique(imageDims, labels, TP());
-x = rand(prod(imageDims)*length(labels))
-@test ss ⊙ x ≈ bss ⊙ x
-println("Passed.")
-
-info("Testing trivial interfaces:")
-imageDims = (8,8)
-labels = [[(i,j) for i in -2:2, j in -2:2]...]
-ss = pairwiseclique4validation(imageDims, labels);
-bss = pairwiseclique(imageDims, labels, TAD());
-pixelNum = prod(imageDims)
-labelNum = length(labels)
-
-@test size(ss) == (pixelNum*labelNum, pixelNum*labelNum)
-@test length(ss) == pixelNum*labelNum*pixelNum*labelNum
-
-@test size(bss) == (pixelNum, labelNum, pixelNum, labelNum) == size(bss.blocks[])
-@test length(bss) == length(bss.blocks[]) == length(ss)
-
-@test nnz(bss)*labelNum^2 == nnz(ss)
-
-println("Passed.")
