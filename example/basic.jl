72960b70bfe750d7ed61cc24cff7f60d7ef22983
Gnimuc <GnimucKey@gmail.com>
2017-02-15 20:56:23 +0800

    Add a basic 3D example

---
+++ example/basic.jl
@@ -0,0 +1,45 @@
+using NRIRHOPM, Interpolations
+#  1  4  7        111  121  131
+#  2  5  8   <=>  211  221  231
+#  3  6  9        311  321  331
+#-----------front--------------
+# 10 13 16        112  122  132
+# 11 14 17   <=>  212  222  232
+# 12 15 18        312  322  332
+#-----------middle-------------
+# 19 22 25        113  123  133
+# 20 23 26   <=>  213  223  233
+# 21 24 27        313  323  333
+#-----------back---------------
+fixed = reshape([1:27;], 3, 3, 3)
+moving = copy(fixed)
+
+moving[1,3,2] = 14
+moving[2,2,2] = 23
+moving[2,2,3] = 25
+moving[1,3,3] = 16
+
+displacements = [(i,j,k) for i in -1:1, j in -1:1, k in -1:1]
+
+warpped, d, spec, energy = multilevel(fixed, moving, [displacements], [(3,3,3)], topology=TP3D(), β=0.1, χ=0.01)
+
+topologyΔ = vecnorm(warpped[end]-fixed)
+originΔ = vecnorm(moving-fixed)
+
+# multilevel
+dim0 = size(fixed)
+dim1 = (6,6,6)
+knots = ntuple(x->linspace(1, dim1[x], dim0[x]), Val{3})
+fixedITP = interpolate(knots, fixed, Gridded(Linear()))
+fixedUp = fixedITP[1:6,1:6,1:6]
+movingITP = interpolate(knots, moving, Gridded(Linear()))
+movingUp = movingITP[1:6,1:6,1:6]
+
+displacements = [(i,j,k) for i in -1:1, j in -1:1, k in -1:1]
+displacementSet = [displacements, displacements]
+gridSet = [dim0, dim1]
+
+warpped, d, spec, energy = multilevel(fixedUp, movingUp, displacementSet, gridSet, topology=TP3D(), β=0.1, χ=0.01)
+
+topologyΔ = vecnorm(warpped[end]-fixedUp)
+originΔ = vecnorm(movingUp-fixedUp)
--- src/multilevel.jl
+++ src/multilevel.jl
@@ -1,4 +1,5 @@
 function multilevel(fixedImg, movingImg, displacementSet, gridSet;
+                    method::AbstractHOPMMethod=MixHOPM(),
                     datacost::DataCost=SAD(), α::Real=1,
                     smooth::SmoothCost=TAD(), β::Real=1,
                     topology::TopologyCost=TP3D(), χ::Real=1
@@ -19,7 +20,7 @@ function multilevel(fixedImg, movingImg, displacementSet, gridSet;
         info(logger, "Level 0: ")
         info(logger, "Image Dimension: $(size(fixedImg))")
         info(logger, "Grid Dimension: $(gridSet[1])")
-        energy[1], spectrums[1] = optimize(fixedImg, movingImg, displacementSet[1], gridSet[1], MixHOPM(), datacost, α, smooth, β, topology, χ)
+        energy[1], spectrums[1] = optimize(fixedImg, movingImg, displacementSet[1], gridSet[1], method, datacost, α, smooth, β, topology, χ)
         indicator = [indmax(spectrums[1][:,i]) for i in indices(spectrums[1],2)]
         displacementFields[1] = fieldlize(indicator, displacementSet[1], gridSet[1])
         warppedImgs[1] = warp(movingImg, displacementFields[1])
@@ -31,11 +32,12 @@ function multilevel(fixedImg, movingImg, displacementSet, gridSet;
         info(logger, "Level $(l-1): ")
         info(logger, "Image Dimension: $(size(fixedImg))")
         info(logger, "Grid Dimension: $(gridSet[l])")
-        energy[l], spectrums[l] = optimize(fixedImg, warppedImgs[l-1], displacementSet[l], gridSet[l], MixHOPM(), datacost, α, smooth, β)
+        energy[l], spectrums[l] = optimize(fixedImg, warppedImgs[l-1], displacementSet[l], gridSet[l], method, datacost, α, smooth, β)
         indicator = [indmax(spectrums[l][:,i]) for i in indices(spectrums[l],2)]
         displacementFields[l] = fieldlize(indicator, displacementSet[l], gridSet[l])
         warppedImgs[l] = warp(warppedImgs[l-1], displacementFields[l])
     end
 
+    info(logger, "Multilevel processing done!")
     return warppedImgs, displacementFields, spectrums, energy
 end
