cd0b83c0c30fdebe337d693995a5e930301d1214
Gnimuc <GnimucKey@gmail.com>
2017-02-19 20:30:00 +0800

    Another minor tweak

--- src/potential.jl
+++ src/potential.jl
@@ -1,5 +1,5 @@
 # unary potentials
-@inline function sum_diff_exp{T,N}(f, fixedImg::AbstractArray{T,N}, movingImg::AbstractArray{T,N}, displacements::AbstractArray{NTuple{N}})
+function sum_diff_exp{T,N}(f, fixedImg::AbstractArray{T,N}, movingImg::AbstractArray{T,N}, displacements::AbstractArray{NTuple{N}})
     imageDims = indices(fixedImg)
     imageDims == indices(movingImg) || throw(DimensionMismatch("fixedImg and movingImg must have the same indices."))
     movingImgITP = interpolate(movingImg, BSpline(Linear()), OnGrid())
@@ -7,7 +7,7 @@
     for a in eachindex(displacements), 𝒊 in CartesianRange(imageDims)
         i = sub2ind(imageDims, 𝒊.I...)
         # Todo: 𝐝 = 𝒊.I .+ displacements[a] (pending julia-v0.6)
-        𝐝 = tuple([𝒊[i]+displacements[a][i] for i = 1:N]...)
+        𝐝 = map(+, 𝒊.I, displacements[a])
         if Base.checkbounds_indices(Bool, indices(movingImg), 𝐝)
             cost[a,i] = e^-f(fixedImg[𝒊] - movingImgITP[𝐝...])
         else
